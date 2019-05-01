f3bb5202499b3ccc99c99ed1f9a9b4200fe5fc63
Viacheslav <viacheslav@mcublog.xyz>
2019-05-02 01:32:32 +0300

    modified test.c/h;

--- Src/init_main/test.c
+++ Src/init_main/test.c
@@ -28,37 +28,46 @@ void test_nand_flash(void)
             return;            
         }
     }
+    printf("Erase check: OK...\r\n");    
     printf("----------------\r\n\r\n");
     
-	for (i = 0; i < PAGE_SIZE; i++) buff[i] = i;
-    
-    printf("----------------\r\n");  
-    printf("Write test buff:\r\n");
-	for (i = 0; i < PAGE_SIZE; i++)
+    for (a = 1 * PAGE_SIZE; a < 5 * PAGE_SIZE; a = a + PAGE_SIZE)
     {
-        buff[i] = i;
-        printf("%#x ", buff[i]);
-        if (i % 16 == 0 && i != 0) printf("\r\n");
-    }
-    printf("\r\n----------------\r\n\r\n");   
-    io_nand_write_8b(a, (uint8_t*)&buff, PAGE_SIZE, 0);
+        printf("Test addres: %#x\r\n", a);
+        for (i = 0; i < PAGE_SIZE; i++) buff[i] = i;
     
-	for (i = 0; i < PAGE_SIZE; i++) buff[i]=0;
-    io_nand_read_8b (a, (uint8_t*) &buff, PAGE_SIZE, 0);
-    
-    printf("----------------\r\n"); 
-    printf("Read test buff and check:\r\n");
-	for (i = 0; i < PAGE_SIZE; i++)
-    {
-        if (buff[i] != i % 256)
+        printf("----------------\r\n");  
+        printf("Write buff...\r\n");
+        for (i = 0; i < PAGE_SIZE; i++)
         {
-            printf("Error read at: %d\r\n", i);
-            return;
+            buff[i] = i;
+            //printf("%#x ", buff[i]);
+            //if (i % 16 == 0 && i != 0) printf("\r\n");
         }
-        printf("%#x ", buff[i]);
-        if (i % 16 == 0 && i != 0) printf("\r\n");
+        io_nand_write_8b(a, (uint8_t*)&buff, PAGE_SIZE, 0);
+        printf("Write buffer: OK...\r\n");
+        printf("----------------\r\n\r\n");
+    
+        for (i = 0; i < PAGE_SIZE; i++) buff[i]=0;
+        io_nand_read_8b (a , (uint8_t*) &buff, PAGE_SIZE, 0);
+    
+        printf("----------------\r\n"); 
+        printf("Read buff and check...\r\n");
+        for (i = 0; i < PAGE_SIZE; i++)
+        {
+            if (buff[i] != i % 256)
+            {
+                printf("Error read at: %d\r\n", i);
+                return;
+            }
+            //printf("%#x ", buff[i]);
+            //if (i % 16 == 0 && i != 0) printf("\r\n");
+        }
+        printf("Read check: OK...\r\n");
+        printf("----------------\r\n\r\n");
     }
-    printf("\r\n----------------\r\n\r\n");
+    
+
     
 }
 //#endif
